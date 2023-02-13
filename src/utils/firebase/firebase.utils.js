640168820442973ce0574f591f5baa87ef61e62b
Adeyemi Olaoye <yhemi06@gmail.com>
2023-02-13 10:12:26 +0100

    added sign-in component and firebase authentication

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,33 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAV5ji6A_3pF3Y9hyHIoIj2rSLvrXKjlJk",
+  authDomain: "crwn-clothing-db-22a58.firebaseapp.com",
+  projectId: "crwn-clothing-db-22a58",
+  storageBucket: "crwn-clothing-db-22a58.appspot.com",
+  messagingSenderId: "405677591707",
+  appId: "1:405677591707:web:4e70a8d85409b1da120a3e",
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters({
+  prompt: "select_account",
+});
+
+export const auth = getAuth();
+
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
