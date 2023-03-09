520386cc0abac42e886cff5a812a02a22c5e43a6
Adeyemi Olaoye <yhemi06@gmail.com>
2023-03-09 10:43:15 +0100

    applied styled component to cart-dropdown, cart-icon and cart-item compoenents...

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -3,7 +3,11 @@ import { useNavigate } from "react-router-dom";
 import { CartContext } from "../../contexts/cart.context";
 import Button from "../button/button.component";
 import CartItem from "../cart-item/cart-item.components";
-import "./cart-dropdown-styles.scss";
+import {
+  CartDropdownContainer,
+  CartItems,
+  EmptyMessage,
+} from "./cart-dropdown-styles";
 
 const CartDropdown = () => {
   const { cartItems } = useContext(CartContext);
@@ -14,17 +18,20 @@ const CartDropdown = () => {
   };
 
   return (
-    <div className="cart-dropdown-container">
-      <div className="cart-items">
-        {cartItems.map((item) => (
-          <CartItem key={item.id} cartItem={item} />
-        ))}
-      </div>
-
+    <CartDropdownContainer>
+      <CartItems>
+        {cartItems.length ? (
+          cartItems.map((item) => <CartItem key={item.id} cartItem={item} />)
+        ) : (
+          <EmptyMessage>Your cart is empty</EmptyMessage>
+        )}
+      </CartItems>
+      {/* {cartItems.length ? ( */}
       <Button onClick={gotoCheckoutHandler}>CHECKOUT</Button>
-
-      {/* <Button className="nav-link" to="/auth"></Button> */}
-    </div>
+      {/* ) : (
+        <span></span>
+      )} */}
+    </CartDropdownContainer>
   );
 };
 
