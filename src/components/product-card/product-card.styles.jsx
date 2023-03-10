b828b20cdef0a61ad316c41065d8db64dd90259e
Adeyemi Olaoye <yhemi06@gmail.com>
2023-03-11 00:16:06 +0100

    added the missing styling

--- src/components/product-card/product-card.styles.jsx
+++ src/components/product-card/product-card.styles.jsx
@@ -4,10 +4,35 @@ export const ProductCartContainer = styled.div`
   width: 100%;
   display: flex;
   flex-direction: column;
-  //@@ -31,21 +33,21 @@
-  display: flex;
-  //}
-  //}
+  height: 350px;
+  align-items: center;
+  position: relative;
+
+  img {
+    width: 100%;
+    height: 95%;
+    object-fit: cover;
+    margin-bottom: 5px;
+  }
+
+  button {
+    width: 80%;
+    opacity: 0.7;
+    position: absolute;
+    top: 255px;
+    display: none;
+  }
+
+  &:hover {
+    img {
+      opacity: 0.8;
+    }
+
+    button {
+      opacity: 0.85;
+      display: flex;
+    }
+  }
 `;
 
 export const Footer = styled.div`
