c958b96815f9765db39f4464ede72a4f49e6a264
Adeyemi Olaoye <yhemi06@gmail.com>
2023-03-10 23:43:27 +0100

    applied stylrf component  to product-card components

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,26 +1,27 @@
-import "./product-card.styles.scss";
+import "./product-card.styles.jsx";
 import Button, { BUTTON_TYPE_CLASSES } from "../button/button.component";
 import { useContext } from "react";
 import { CartContext } from "../../contexts/cart.context";
+import { Footer, Name, Price, ProductCartContainer } from "./product-card.styles.jsx";
 
 const ProductCard = ({ product }) => {
   const { addItemToCart } = useContext(CartContext);
   const { name, price, imageUrl } = product;
   const addProductToCart = () => addItemToCart(product);
   return (
-    <div className="product-card-container">
+     <ProductCartContainer>
       <img src={imageUrl} alt={`${name}`} />
-      <div className="footer">
-        <span className="name">{name}</span>
-        <span className="price">{price}</span>
-      </div>
+      <Footer>
+        <Name>{name}</Name>
+        <Price>{price}</Price>
+      </Footer>
       <Button
         buttonType={BUTTON_TYPE_CLASSES.inverted}
         onClick={addProductToCart}
       >
-        Add to cart
+        Add to card
       </Button>
-    </div>
+    </ProductCartContainer>
   );
 };
 
---
+++ src/components/product-card/product-card.styles.jsx
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+export const ProductCartContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  //@@ -31,21 +33,21 @@
+  display: flex;
+  //}
+  //}
+`;
+
+export const Footer = styled.div`
+  width: 100%;
+  height: 5%;
+  display: flex;
+  justify-content: space-between;
+  font-size: 18px;
+`;
+
+export const Name = styled.span`
+  width: 90%;
+  margin-bottom: 15px;
+`;
+
+export const Price = styled.span`
+  width: 10%;
+`;
