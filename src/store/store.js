8f161618ee6b7e17d90e45f440a628267a3c50a2
Adeyemi Olaoye <yhemi06@gmail.com>
2023-03-13 10:51:48 +0100

    finished setting up of redux store

--- src/store/store.js
+++ src/store/store.js
@@ -1,3 +1,9 @@
 import { compose, createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
+import { rootReducer } from "./root-reducer";
 
+//when you dispatch actions, before action hits the reducers, it hits the middleware first
+//it logs out the state
+const middleWares = [logger];
+const composedEnhancers = compose(applyMiddleware(...middleWares));
+export const store = createStore(rootReducer, undefined, composedEnhancers);
