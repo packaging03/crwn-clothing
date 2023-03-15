8e627c08d8275fd741b97cc8c24c361acfffe753
Adeyemi Olaoye <yhemi06@gmail.com>
2023-03-15 14:24:14 +0100

    Revert "sta"
    This reverts commit 4e334ef96469036f661282246ef474c7daa4b0ec.

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -6,16 +6,15 @@ import {
   NavLinksContainer,
   NavLin,
 } from "./navigation.styles";
-// import { UserContext } from "../../contexts/user.context";
+import { UserContext } from "../../contexts/user.context";
 import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
 import { signOutUser } from "../../utils/firebase/firebase.utils";
 import CartIcon from "../../components/cart-icon/cart-icon.component";
 import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 import { CartContext } from "../../contexts/cart.context";
-import { useSelector } from "react-redux";
 
 const Navigation = () => {
-  const currentUser =  useSelector((state) => state.user.currentUser);
+  const { currentUser } = useContext(UserContext);
   const { isCartOpen } = useContext(CartContext);
   return (
     <Fragment>
