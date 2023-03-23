83b51d43eb53b74be34c96b040387f35a3520d88
Adeyemi Olaoye <yhemi06@gmail.com>
2023-03-23 02:13:17 +0100

    added reselect library, memorized selector.

--- src/store/store.js
+++ src/store/store.js
@@ -4,6 +4,21 @@ import { rootReducer } from "./root-reducer";
 
 //when you dispatch actions, before action hits the reducers, it hits the middleware first
 //it logs out the state
-const middleWares = [logger];
+
+const loggerMiddleware = (store) => (next) => (action) => {
+  if (!action.type) {
+    return next(action);
+  }
+
+  // console.log("type", action.type);
+  // console.log("payload", action.payload);
+
+  next(action);
+
+  // console.log("next state: ", store.getState());
+};
+
+const middleWares = [loggerMiddleware];
+
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 export const store = createStore(rootReducer, undefined, composedEnhancers);
